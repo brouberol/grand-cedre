66c556632fdb55e3ee1f3469011629bd00afa8ef
Balthazar Rouberol <br@imap.cc>
2019-08-26 20:21:06 +0200

    Add all models to the admin, with some custom rendering

--- app.py
+++ app.py
@@ -2,16 +2,40 @@ from flask import Flask
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
+from markupsafe import Markup
 
 from grand_cedre.models.client import Client
 from grand_cedre.models.contract import Contract
+from grand_cedre.models.room import Room
+from grand_cedre.models.booking import Booking
 
 
 app = Flask("grand-cedre")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/data.db"
 app.config["FLASK_ADMIN_SWATCH"] = "flatly"
+app.config[
+    "SECRET_KEY"
+] = b"\ry\xe0\x97\xe88\xed\x84\x05\xfdfN\x1daQ\xf8\x83!\xeanp\x80R\xd1"
+
 db = SQLAlchemy(app)
 admin = Admin(app, name="grand-cedre", template_mode="bootstrap3")
 
+
+class BookingView(ModelView):
+    column_formatters = {
+        "calendar_link": (
+            lambda v, c, m, p: Markup(f"<a href={m.calendar_link}>link</a>")
+        ),
+        "start_datetime": lambda v, c, m, p: (
+            f"{m.start_datetime.date()}:{m._format_time(m.start_datetime.time(), sep=':')}"
+        ),
+        "end_datetime": lambda v, c, m, p: (
+            f"{m.end_datetime.date()}:{m._format_time(m.end_datetime.time(), sep=':')}"
+        ),
+    }
+
+
 admin.add_view(ModelView(Client, db.session))
 admin.add_view(ModelView(Contract, db.session))
+admin.add_view(BookingView(Booking, db.session))
+admin.add_view(ModelView(Room, db.session))
