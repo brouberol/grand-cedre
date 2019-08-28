ec4eac53fb9121442c1df6936615d46f530f055d
Balthazar Rouberol <br@imap.cc>
2019-08-28 12:14:26 +0200

    Initialization of all models

--- grand_cedre/models/__init__.py
+++ grand_cedre/models/__init__.py
@@ -1,3 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+from .booking import Booking
+from .client import Client
+from .invoice import Invoice
+from .contract import Contract
+from .room import Room
