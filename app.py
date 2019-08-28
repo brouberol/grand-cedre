f200c69c9f02de8a06a235d2d25576ca5a66125f
Balthazar Rouberol <br@imap.cc>
2019-08-28 09:54:46 +0200

    Translate admin interface

--- app.py
+++ app.py
@@ -22,30 +22,81 @@ db = SQLAlchemy(app)
 admin = Admin(app, name="grand-cedre", template_mode="bootstrap3")
 
 
+class ClientView(ModelView):
+    column_exclude_list = ["is_owner"]
+    column_searchable_list = ("first_name", "last_name")
+    form_excluded_columns = ("contracts", "bookings", "invoices")
+    column_labels = {
+        "first_name": "Prénom",
+        "last_name": "Nom",
+        "address": "Adresse",
+        "zip_code": "Code postal",
+        "city": "Ville",
+    }
+
+
+class ContractView(ModelView):
+    column_searchable_list = ("client.first_name", "client.last_name")
+
+    column_labels = {
+        "client": "Client",
+        "start_date": "Date de début",
+        "end_date": "Date de fin",
+        "booking_duration": "Durée du créneau",
+        "hourly_rate": "Taux horaire",
+    }
+
+
 class BookingView(ModelView):
+    column_labels = {
+        "client": "Client",
+        "room": "Salle",
+        "invoice": "Facture",
+        "start_date": "Date",
+        "start_time": "Heure de début",
+        "end_time": "Heure de fin",
+        "calendar_link": "Lien",
+        "price": "Prix",
+        "frozen": "Consolidée",
+    }
+    column_list = column_labels.keys()
+
+    column_searchable_list = (
+        "client.first_name",
+        "client.last_name",
+        "room.name",
+        "start_datetime",
+    )
     column_formatters = {
         "calendar_link": (
-            lambda v, c, m, p: Markup(f"<a href={m.calendar_link}>link</a>")
+            lambda v, c, m, p: Markup(f"<a href={m.calendar_link}>lien</a>")
         ),
-        "start_datetime": lambda v, c, m, p: (
-            f"{m.start_datetime.date()}:{m._format_time(m.start_datetime.time(), sep=':')}"
+        "start_date": lambda v, c, m, p: (f"{m.start_datetime.date()}"),
+        "start_time": lambda v, c, m, p: (
+            f"{m._format_time(m.start_datetime.time(), sep=':')}"
         ),
-        "end_datetime": lambda v, c, m, p: (
-            f"{m.end_datetime.date()}:{m._format_time(m.end_datetime.time(), sep=':')}"
+        "end_time": lambda v, c, m, p: (
+            f"{m._format_time(m.end_datetime.time(), sep=':')}"
         ),
     }
 
 
 class InvoiceView(ModelView):
+    column_searchable_list = ("client.first_name", "client.last_name")
     column_list = ("client", Invoice.period, "total_price", Invoice.issued_at)
+    column_labels = {
+        "client": "Client",
+        "period": "Période",
+        "total_price": "Total",
+        "issued_at": "Date d'édition",
+    }
     column_formatters = {
         "client": (lambda v, c, m, p: f"{m.client}"),
         "total_price": (lambda v, c, m, p: f"{m.total_price}{m.symbol}"),
     }
 
 
-admin.add_view(ModelView(Client, db.session))
-admin.add_view(ModelView(Contract, db.session))
-admin.add_view(BookingView(Booking, db.session))
-admin.add_view(ModelView(Room, db.session))
-admin.add_view(InvoiceView(Invoice, db.session))
+admin.add_view(ClientView(Client, db.session, "Clients"))
+admin.add_view(ContractView(Contract, db.session, "Contrats"))
+admin.add_view(BookingView(Booking, db.session, "Réservations"))
+admin.add_view(InvoiceView(Invoice, db.session, "Factures"))
