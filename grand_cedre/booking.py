5f25c348c9a9bcef0c59adc88e3acd41449e0784
Balthazar Rouberol <br@imap.cc>
2019-09-01 14:50:03 +0200

    Fix pricing search query to include the 8+ pricing

--- grand_cedre/booking.py
+++ grand_cedre/booking.py
@@ -123,7 +123,10 @@ def get_daily_booking_pricing(daily_booking, contract, individual_status, sessio
         pricing = session.query(pricing_model).filter(
             and_(
                 pricing_model.duration_from < daily_booking.duration_hours,
-                pricing_model.duration_to >= daily_booking.duration_hours,
+                or_(
+                    pricing_model.duration_to >= daily_booking.duration_hours,
+                    pricing_model.duration_to.is_(None),
+                ),
                 pricing_model.valid_from <= daily_booking.date,
                 or_(
                     pricing_model.valid_to.is_(None),
