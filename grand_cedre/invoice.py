5a1002fcf34150097e18b012ba35b3ecc3898ccb
Balthazar Rouberol <br@imap.cc>
2019-10-01 07:58:15 +0200

    Commit invoice by invoice, as we need the invoice PK ID to exist to generate the invoice human readable ID

--- grand_cedre/invoice.py
+++ grand_cedre/invoice.py
@@ -76,13 +76,11 @@ def generate_invoice_per_contract(
             currency="EURO",
         )
         if created:
-            # Hack: if we've created the invoice for the first time, the relationship
-            # can't resolve just yet
-            invoice.contract = contract
             for booking in bookings:
                 booking.invoice = invoice
                 booking.frozen = True
                 session.add(booking)
+            session.commit()
             logger.info(f"Invoice for {invoice.total_price}{invoice.symbol} generated")
         else:
             logger.info(f"Invoice {invoice} already was generated")
